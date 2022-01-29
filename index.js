d967a27436bf5e0e7da2c4ac8ff514a5ea23aae5
Pablo Trindade <pablo.trindade@cwi.com.br>
2022-01-29 01:44:36 -0300

    add map

---
+++ index.js
@@ -0,0 +1,5 @@
+const elImgSaltyPizza = document.getElementById("saltyPizza");
+const elImgCandyPizza = document.getElementById("candyPizza");
+
+console.log(elImgCandyPizza)
+console.log(elImgSaltyPizza)
